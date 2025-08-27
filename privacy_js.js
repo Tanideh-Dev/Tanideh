2bdfbf2e43a276b7fdcd59319b85e9225d5fbefa
Tanideh-Dev <HelgaAndersdotter@proton.me>
2025-08-27 11:13:53 +0330

    Update privacy_js.js

--- privacy_js.js
+++ privacy_js.js
@@ -1,5 +1,18 @@
 const toggleBtn = document.getElementById('lang-toggle');
+const title = document.getElementById("site-title");
+
 toggleBtn.addEventListener('click', () => {
+
+  // trigger flash animation
+  langBtn.classList.remove("flash");
+  void langBtn.offsetWidth; // force reflow to restart animation
+  langBtn.classList.add("flash");
+
+// change title language
+  isEnglish = !isEnglish;
+  title.textContent = isEnglish ? "Tanideh" : "تنیده";
+
+// change privacy language
     document.querySelectorAll('.en, .fa').forEach(el => {
         el.style.display = (el.style.display === 'none' || el.style.display === '') ? 'block' : 'none';
     });
